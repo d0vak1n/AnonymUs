dd467908405c8e7149d1a16b1c3bdebd5fce5ac9
Raul <rulanxa@gmail.com>
2023-06-03 19:25:17 +0200

    chat funcional sin anonimato

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-import express from 'express';
-import http from 'http';
-
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', (req:any, res:any) => {
-  res.sendFile(__dirname + '/html/index.html');
+const io = new Server(server);
+
+app.get("/", (req: any, res: any) => {
+  res.sendFile(__dirname + "/html/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
